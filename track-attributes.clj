30c412f4f52bfa3d1bdb6898fa6b650c0a8f6e8f
higg <higginshiggins2@protonmail.com>
2021-07-18 01:53:21 -0700

    first commit of clojure re-write

---
+++ track-attributes.clj
@@ -0,0 +1,11 @@
+(def car-stats-2018 
+	"Dictionary of car attributes for 2018 model in relation to rest of the grid (9 teams)"
+	{'overall 3, 'top-speed 3, 'acceleration 3, 'deceleration 4, 'low-speed-corners 1, 'medium-speed-corners 4, 'high-speed-corners 2})
+
+(def chassis-make-up-2018 
+	"Dictionary of chassis stats determined at the beginning of the season (2018). 5 stars max, approx. floating-point rating"
+	{'tyre-wear 3.4, 'tyre-heating 3.1, 'fuel-efficiency 2.4, 'improvability 2.4})
+
+(def chassis-suppliers-2018
+	"Dictionary of ratings and figures of 2018 chassis suppliers"
+	{'all-suppliers {'engine-supplier 'rossini, 'fuel-efficiency 'medium, 'improvability 'medium, 'base-stat-modifier 13, 'cost 10.2e6}})
